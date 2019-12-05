806b92696dbff09387c69ea2b9bb471be5842444
dalmeidg <dalmeidg@coventry.ac.uk>
2019-12-05 09:24:22 +0000

    Changed display.spec.js
    Changed the structures of some tests

--- unit tests/display.spec.js
+++ unit tests/display.spec.js
@@ -1,82 +1,43 @@
 
 'use strict'
 
-const sqlite = require('sqlite-async')
-
+const User = require('../modules/user.js')
 const Display = require('../modules/display.js')
 
 describe('list()', () => {
 
-	test('list items', async done => {
+	test('list items when there are no items', async done => {
 		expect.assertions(1)
-		const rightResults = []
-		let obj = {title: 'title3', price: 3, id: 3}
-		rightResults.push(obj)
-		obj = {title: 'title2', price: 2, id: 2}
-		rightResults.push(obj)
-		obj = {title: 'title1', price: 1, id: 1}
-		rightResults.push(obj)
-
-		//Arranges the in-memory database with the needed table
-		const db = await sqlite.open(':memory:')
-		console.log(typeof db)
-		const sqlItemsTable1 = '(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, '
-		const sqlItemsTable2 = 'shortDesc TEXT, longDesc TEXT, price INTEGER, owner TEXT);'
-		const sqlItemsTable = 'CREATE TABLE IF NOT EXISTS items '
-		const sqlItems = `${sqlItemsTable} ${ sqlItemsTable1}${ sqlItemsTable2}`
-		await db.run(sqlItems)
-		//Arranges the in-memory database by populating it with data
-		const sql = 'INSERT INTO items(title, shortDesc, longDesc, price, owner) '
-		let sql2 = 'VALUES("title1", "short1", '
-		let sql3 = '"long1", "1", "owner1")'
-		await db.run(sql + sql2 + sql3)
-		sql2 = 'VALUES("title2", "short2", '
-		sql3 = '"long2", "2", "owner2")'
-		await db.run(sql + sql2 + sql3)
-		sql2 = 'VALUES("title3", "short3", '
-		sql3 = '"long3", "3", "owner3")'
-		await db.run(sql + sql2 + sql3)
-
-		const display = await new Display()
-		const list = await display.list(db)
-		expect(list).toEqual(rightResults)
+		const display = await new Display('test1.db')
+		const list = await display.list()
+		expect(list).toEqual([])
 		done()
 	})
 })
 
 describe('details()', () => {
 
-	test('Get details for a particular item ID', async done => {
+	test('Get details for a non-existing item', async done => {
 		expect.assertions(1)
-		let rightResults = {}
-		rightResults = {title: 'title3', price: 3, id: 3, longDesc: 'long3', owner: 'owner3'}
 
-
-		//Arranges the in-memory database with the needed table
-		const db = await sqlite.open(':memory:')
-		console.log(typeof db)
-		const sqlItemsTable1 = '(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, '
-		const sqlItemsTable2 = 'shortDesc TEXT, longDesc TEXT, price INTEGER, owner TEXT);'
-		const sqlItemsTable = 'CREATE TABLE IF NOT EXISTS items '
-		const sqlItems = `${sqlItemsTable} ${ sqlItemsTable1}${ sqlItemsTable2}`
-		await db.run(sqlItems)
-		//Arranges the in-memory database by populating it with data
-		const sql = 'INSERT INTO items(title, shortDesc, longDesc, price, owner) '
-		let sql2 = 'VALUES("title1", "short1", '
-		let sql3 = '"long1", "1", "owner1")'
-		await db.run(sql + sql2 + sql3)
-		sql2 = 'VALUES("title2", "short2", '
-		sql3 = '"long2", "2", "owner2")'
-		await db.run(sql + sql2 + sql3)
-		sql2 = 'VALUES("title3", "short3", '
-		sql3 = '"long3", "3", "owner3")'
-		await db.run(sql + sql2 + sql3)
-
-		const display = await new Display()
-		const details = await display.details(db, 3)
-		expect(details).toEqual(rightResults)
+		const display = await new Display('test2.db')
+		const details = await display.details(3)
+		expect(details).toEqual()
 		done()
 	})
 
+	test('Get details for an item', async done => {
+		expect.assertions(1)
+
+		const display = await new Display('test2.db')
+		const user = await new User('test2.db')
+		await user.register('doej', 'password', 'email@email.com', '123456789', 'paypal')
+		const itemInfo = {title: 't',shortDesc: 's',
+			longDesc: 'l',price: 12, owner: 'o'}
+		await user.uploadItem([],itemInfo)
+		const details = await display.details(1)
+		expect(details).toEqual()
+		done()
+	})
 
 })
